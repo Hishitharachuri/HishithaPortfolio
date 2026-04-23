37bbe26962183ec3bd15e4383b7f220523b7669e
HishithaRachuri <hishithrachuri@gmail.com>
2026-04-23 18:03:34 +0530

    Portfolio code

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import "../App.css";
 import github from "../assets/github.png";
-import linkedin from "../assets/linkedIn.png";
+import linkedin from "../assets/LinkedIn.png";
 import mail from "../assets/mail.png";
 
 const contact = [
